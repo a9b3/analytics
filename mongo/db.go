469bd314fefdfbf98a1d62b22834ff34f56a3da5
Sam Lau <esayemm@gmail.com>
2018-03-02 01:58:25 -0800

    add mongo connection

---
+++ mongo/db.go
@@ -0,0 +1,17 @@
+package mongo
+
+import (
+	mgo "gopkg.in/mgo.v2"
+)
+
+var db *mgo.Database
+
+// Init sets package wide db reference
+func Init(uri, name string) {
+	session, err := mgo.Dial(uri)
+	if err != nil {
+		panic(err)
+	}
+
+	db = session.DB(name)
+}
--- server.go
+++ server.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 
 	"github.com/esayemm/analytics/config"
+	"github.com/esayemm/analytics/mongo"
 	"github.com/esayemm/analytics/v1"
 	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
 	cfg := config.New()
+	mongo.Init(cfg.DB_URI, cfg.DB_NAME)
 
 	if cfg.APP_ENV == "dev" {
 		fmt.Printf("configs: %+v\n", cfg)
