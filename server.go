553da814dda7a3f4700e6cd7bb6859bbfd25d2ea
Sam Lau <esayemm@gmail.com>
2018-03-03 00:29:57 -0800

    use logrus for logging

--- server.go
+++ server.go
@@ -1,27 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
+	"github.com/Sirupsen/logrus"
+	"github.com/esayemm/analytics/api"
 	"github.com/esayemm/analytics/config"
 )
 
 func main() {
 	cfg, err := config.New()
 	if err != nil {
-		panic(err)
+		logrus.WithError(err).Fatalf("config init %s", err.Error())
+		return
 	}
 	// db := mongo.Init(cfg.DB_URI, cfg.DB_NAME)
 
 	if cfg.APP_ENV == "dev" {
-		fmt.Printf("configs: %+v\n", cfg)
+		logrus.Infof("configs: %+v\n", cfg)
 	}
 
-	// r, err := api.New(cfg)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	//
-	// fmt.Println("listening on port", cfg.PORT)
-	// http.ListenAndServe(":"+cfg.PORT, r)
+	r, err := api.New(*cfg)
+	if err != nil {
+		logrus.WithError(err).Fatalf("api init %s", err.Error())
+		return
+	}
+
+	logrus.Infof("listening on port %s", cfg.PORT)
+	if err := http.ListenAndServe(":"+cfg.PORT, r); err != nil {
+		logrus.WithError(err).Fatalf("server init %s", err.Error())
+		return
+	}
 }
